4975072ab120128cf6abc08e869de384aa79c049
Lorenzo <lorenzo.foschetti00@gmail.com>
2024-03-08 16:14:35 +0100

    ad an attempt for es 9

--- D5.js
+++ D5.js
@@ -95,7 +95,7 @@ for (let i = 0; i < cars.lenght; i++) {
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 for (let i = 0; i < cars.lenght; i++) {
-  if (cars.color[0] === "b") console.log("Fizz");
+  if (cars[i].color[0] === "b") console.log("Fizz");
   else {
     console.log("Buzz");
   }
@@ -107,6 +107,11 @@ for (let i = 0; i < cars.lenght; i++) {
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
+let i = 0;
+while (i !== 32) {
+  console.log(numericArray[i]);
+  i++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
